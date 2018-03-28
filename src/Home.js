92c2eb0dca1416c709bdfe6e3eb4a6af27ddde45
Alexandre Eisenmann <alke@Alexandres-MacBook-Pro.local>
2018-03-28 23:08:55 +1100

    tweak

--- src/Home.js
+++ src/Home.js
@@ -22,7 +22,7 @@ class Home extends Component {
 
 
   flagSvg(iso2code) {
-    return <div style={{width:"80px",height: "60px", background:`url(${this.flags(`./${iso2code}.svg`)}) no-repeat top left`,backgroundSize: "contain"}}></div>
+    return <div style={{width:"60px",height: "45px", background:`url(${this.flags(`./${iso2code}.svg`)}) no-repeat top left`,backgroundSize: "contain"}}></div>
 
   }
 
@@ -37,14 +37,14 @@ class Home extends Component {
             <div style={{
               margin: "auto", 
               color: "black",
-              fontSize: "50px",
+              fontSize: "30px",
               fontFamily: "Playfair Display",
               textAlign: "center",
               marginLeft: "50px",
               marginRight: "50px",
             }}><p> Bolão <span style={{ fontFamily: "Lato"}}>EISENMANN</span> da Copa do Mundo 2018</p></div>
 
-            <div style={{width: "100%", paddingTop:  "20px", paddingBottom: "20px", backgroundColor: amberA700}}>
+            <div style={{width: "100%", paddingTop:  "0px", paddingBottom: "20px", backgroundColor: amberA700}}>
     
             <div style={{paddingTop: "50px", margin: "auto", textAlign: "center", display: "inline-block"}}>
               <div style={{float: "left"}}>{this.flagSvg("br")}</div>
@@ -55,7 +55,7 @@ class Home extends Component {
             </div>
 
               <div style={{width: "70%", margin: "auto"}}>
-                <p style={{fontFamily: "Playfair Display",fontSize: "30px", textAlign: "center"}}>
+                <p style={{fontFamily: "Playfair Display",fontSize: "20px", textAlign: "center"}}>
 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ullamcorper sodales velit a venenatis. Aliquam hendrerit pulvinar turpis, et rutrum neque mollis at. Nullam consectetur vulputate arcu eget venenatis. Nam sed sodales velit. Morbi tortor massa, lacinia sed tellus vel, finibus interdum sapien. Proin dolor justo, hendrerit in pulvinar vel, porttitor vitae purus. Curabitur in justo sit amet lacus placerat convallis. Proin in iaculis mauris. Integer sit amet dictum metus. In nec enim feugiat, imperdiet leo eget, suscipit erat.</p>
               </div>
               
