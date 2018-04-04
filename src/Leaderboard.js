ca9dc24788a46384a01ed2383ab4f0802b345ee9
Alexandre Eisenmann <alke@Alexandres-MacBook-Pro.local>
2018-04-04 22:05:52 +1000

    add calculation

--- src/Leaderboard.js
+++ src/Leaderboard.js
@@ -7,6 +7,7 @@ import CircularProgress from 'material-ui/CircularProgress';
 import 'react-sticky-table/dist/react-sticky-table.css';
 import {green700, blue600, cyan500,cyan600,cyan100, cyan200, cyan300, pink500,pink100} from 'material-ui/styles/colors';
 import * as firebase from 'firebase'
+import queryString from 'query-string'
 
 
 
@@ -26,22 +27,78 @@ export default class Leaderboard extends Component {
     this.state = {games: []}
 
     const matches = ['a','b','c','d','e','f','g','h'].map((group) => data.groups[group].matches).reduce((acc,ele) => acc.concat(ele),[])
+    const sortedMatches = matches.sort((a,b) => {
+      if (moment(a.date).isBefore(moment(b.date) )) 
+        return -1
+      else if (moment(a.date).isAfter(moment(b.date)))
+        return 1 
+      return 0
+    })
+    this.queryString = queryString.parse(props.location.search)
+    Object.keys(this.queryString).map((matchIdx) => {
+      const result = this.queryString[matchIdx].split('x')
+      sortedMatches[matchIdx-1].home_result = parseInt(result[0],0)
+      sortedMatches[matchIdx-1].away_result = parseInt(result[1],0)
+    })
+
+
+
 
     this.state = {games: [],
-       matches: matches.sort((a,b) => {
-        if (moment(a.date).isBefore(moment(b.date) )) 
-          return -1
-        else if (moment(a.date).isAfter(moment(b.date)))
-          return 1 
-        return 0
-    })
+       matches: sortedMatches
     }
-
 
 
     this.teams = data.teams.reduce((acc,ele) => {acc[ele.id] = ele; return acc}, {})
     this.flags = require.context("./flags/4x3/", false, /.*\.svg$/);
 
+  }
+
+
+  calculatePosition(sortedGames) {
+    if (sortedGames.length <= 0)
+      return
+
+    let k = 1
+    let pos = 1
+    let total = sortedGames[0].total
+    sortedGames.map((game) => {
+        if (game.total != total) {
+          pos = k
+          total = game.total
+        } 
+        game.position = pos
+        k++
+    })
+  }
+
+
+  calculate(games, matches) {
+    games.map((game) => {
+
+      let total = 0
+      matches.map((match) => {
+
+        const rh = match.home_result
+        const ra = match.away_result
+
+        if (rh && ra) {
+
+            const h = game[match.name].h 
+            const a = game[match.name].a
+            if (h == rh && a == ra) {
+              game[match.name].pts = 8
+            } else if (rh-ra == h-a ) {
+              game[match.name].pts = 5
+            } else if (Math.sign(rh-ra) == Math.sign(h-a)) {
+              game[match.name].pts = 3
+            }
+            total += game[match.name].pts || 0 
+
+        }
+      })
+      game["total"] = total
+    })
   }
 
 
@@ -78,21 +135,25 @@ export default class Leaderboard extends Component {
         
       });
 
-      
-
-      
-      this.setState({games: games.sort((a,b) => {
-        var nameA = a.name.toUpperCase()
-        var nameB = b.name.toUpperCase() 
-        if (nameA < nameB) {
-          return -1;
+      this.calculate(games, this.state.matches)
+      const sortedGames = games.sort((a,b) => {
+        const diff = b.total - a.total
+        if (diff != 0) return diff 
+        else {
+          var nameA = a.name.toUpperCase()
+          var nameB = b.name.toUpperCase() 
+          if (nameA < nameB) {
+            return -1;
+          }
+          if (nameA > nameB) {
+            return 1;
+          }
+        
+          return 0;
         }
-        if (nameA > nameB) {
-          return 1;
-        }
-      
-        return 0;
-      })})
+      })
+      this.calculatePosition(sortedGames)
+      this.setState({games: sortedGames})
       
     })
     
@@ -108,23 +169,25 @@ export default class Leaderboard extends Component {
     header.push(<Cell key={"nome"}></Cell>)
     this.state.matches.map((match,i) => {
       header.push(<Cell key={`ha${i}`}>
-        <div style={{marginBottom: "6px", width: "40px"}}>
-        <div style={{color: "black", textAlign: "center", paddingLeft: "7px", fontSize: "small", fontWeight: "bold", fontFamily: "Lato",  height: "60px", textOrientation: "upright", writingMode: "vertical-rl"}}>
+        <div style={{position: "relative", marginTop: "6px", marginBottom: "6px", marginLeft: "4px", marginRight: "4px"  }}>
+        <div style={{color: "black", textAlign: "center", paddingLeft: "7px", fontSize: "small", fontWeight: "bold", fontFamily: "Lato", textOrientation: "upright", writingMode: "vertical-rl"}}>
           {this.teams[match.home_team].name.substring(0,3).toUpperCase()}
         </div>
-        <div >{this.flagSvg(this.teams[match.home_team].iso2)}</div>
+        <div style={{ marginTop: "4px"}}>{this.flagSvg(this.teams[match.home_team].iso2)}</div>
+        <div style={{ marginTop: "4px", textAlign: "center"}}>{match.home_result ? match.home_result : "."}</div>
         </div>
       </Cell>)
       header.push(<Cell key={`hb${i}`}>
-        <div style={{marginBottom: "6px",width: "40px"}}>
-        <div style={{color: "black", textAlign: "center", paddingLeft: "7px", fontSize: "small", fontWeight: "bold", fontFamily: "Lato", height: "60px", textOrientation: "upright", writingMode: "vertical-rl"}}>
+        <div style={{position: "relative", marginBottom: "6px", marginLeft: "4px", marginRight: "4px"  }}>
+        <div style={{color: "black",textAlign: "center", paddingLeft: "7px", fontSize: "small", fontWeight: "bold", fontFamily: "Lato",  textOrientation: "upright", writingMode: "vertical-rl"}}>
           {this.teams[match.away_team].name.substring(0,3).toUpperCase()}
         </div>
-        <div >{this.flagSvg(this.teams[match.away_team].iso2)}</div>
+        <div style={{ marginTop: "4px"}}>{this.flagSvg(this.teams[match.away_team].iso2)}</div>
+        <div style={{ marginTop: "4px", textAlign: "center"}}>{match.away_result ? match.away_result : "."}</div>
         </div>
       </Cell>)
       header.push(<Cell key={`hc${i}`}>
-        <div style={{marginBottom: "6px",width: "40px"}}>
+        <div style={{marginBottom: "6px", width: "40px"}}>
         <div style={{color: "black", textAlign: "center", paddingLeft: "7px", fontSize: "small", fontWeight: "bold", fontFamily: "Lato", height: "60px", textOrientation: "upright", writingMode: "vertical-rl"}}>
         </div>
         </div>
@@ -134,12 +197,18 @@ export default class Leaderboard extends Component {
     const rows=[]
     this.state.games.map((game,i) => {
       const row = []
-      row.push(<Cell style={{  paddingTop: "10px", paddingBottom: "10px", paddingRight: "10px"}} key={`g${i}`} ><div >{game.name}</div></Cell>)
+      row.push(<Cell style={{  paddingTop: "10px", paddingBottom: "10px", paddingRight: "10px"}} key={`g${i}`} >
+        <div >
+          <span style={{ float: "left", width: "20px", textAlign: "right"}}>{game.position}</span>
+          <span style={{marginLeft: "10px",float: "left"}}>{game.name}</span>
+          <span style={{marginLeft: "2px",float: "right"}}> {game.total}</span>
+        </div>
+        </Cell>)
       this.state.matches.map((match,j) => {
 
         row.push(<Cell key={`ra${i}-${j}`} style={{color: "rgba(50, 50, 50, 0.9)", paddingTop: "10px", fontFamily: "Lato",textAlign: "center"}}>{game[match.name].h}</Cell>)
         row.push(<Cell key={`rb${i}-${j}`} style={{color: "rgba(50, 50, 50, 0.9)", paddingTop: "10px", fontFamily: "Lato",textAlign: "center"}}>{game[match.name].a}</Cell>)
-        row.push(<Cell key={`rc${i}-${j}`} style={{color: "rgba(50, 50, 50, 0.9)", paddingTop: "10px", fontFamily: "Lato",textAlign: "center"}}></Cell>)
+        row.push(<Cell key={`rc${i}-${j}`} style={{color: "rgba(50, 50, 50, 0.9)", paddingTop: "10px", fontFamily: "Lato",textAlign: "center"}}>{game[match.name].pts}</Cell>)
       })
       rows.push(row)
     })
